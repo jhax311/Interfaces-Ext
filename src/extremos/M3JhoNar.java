75203c998e9ec884adb38e429cda2f7800e64e10
JANV <81357115+jhax311@users.noreply.github.com>
2023-03-20 09:44:05 +0100

    n

--- src/extremos/M3JhoNar.java
+++ src/extremos/M3JhoNar.java
@@ -6,53 +6,51 @@ package extremos;
 public final class M3JhoNar implements IntEJhoNar{
 
   /**
-   * 
-   * @param a
+   * genera un entero con el valor maximo, 
+   * para que en la comparacion recoja cualquier
+   * hace un bucle, comparando los nums del array
+   * y guarda el más pequeño
+   * @param array
    * @return número entero mínimo
    */
-    /*
-    genera un entero con el valor maximo, 
-    para que en la comparacion recoja cualquier
-    hace un bucle, comparando los nums del array
-    y guarda el más pequeño
-    */
+
    @Override
-    public int min(int[] a) {
+    public int min(int[] array) {
        int min= Integer.MAX_VALUE;
-        for (int i : a) { min= Math.min(min, i); }
+        for (int i : array) { min= Math.min(min, i); }
        return min;
     }
   /**
    * 
-   * @param a
+   * @param array
    * @return número entero máximo
    */
     @Override
-    public int max(int[] a) {
+    public int max(int[] array) {
        int max= Integer.MIN_VALUE;
-        for (int i : a) { max= Math.max(max, i); }
+        for (int i : array) { max= Math.max(max, i); }
        return max;
     }
   /**
    * 
-   * @param a
+   * @param array
    * @return número decimal mínimo
    */
     @Override
-    public double min(double[] a) {
+    public double min(double[] array) {
        double min= Double.MAX_VALUE;
-        for (double i : a) { min= Math.min(min, i); }
+        for (double i : array) { min= Math.min(min, i); }
        return min;
     }
   /**
    * 
-   * @param a
+   * @param array
    * @return número decimal máximo
    */
     @Override
-    public double max(double[] a) {
+    public double max(double[] array) {
        double max= Double.MIN_VALUE;
-        for (double i : a) { max= Math.max(max, i); }
+        for (double i : array) { max= Math.max(max, i); }
        return max;
     }
 
